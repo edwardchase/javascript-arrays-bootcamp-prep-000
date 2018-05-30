7fe9f60a9ff9651ff38ad682c0fcc2e6f185453b
Edward Mixon <edwardchase@gatech.edu>
2018-05-30 23:59:05 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,25 @@
+var chocolateBars = [
+  'snickers',
+  'hundred grand',
+  'kitkat',
+  'skittles'];
+  
+  function addElementToBeginningOfArray(array,element) {
+    array = [element,...array]
+    return array
+  }
+  
+  function destructivelyAddElementToBeginningOfArray(array,element) {
+  array = [element,array.shift()]
+    return array
+  }
+  
+    function addElementToEndOfArray(array,element) {
+    array = [...array,element]
+    return array
+  }
+  
+  function destructivelyAddElementToEndOfArray(array,element) {
+    array[end] = element
+    return array
+  }
